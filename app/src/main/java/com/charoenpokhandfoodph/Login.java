2d9050cf58b36617b975d2f60bc31f091d2a7369
ChickenstarBranch <japjaprazo@gmail.com>
2021-07-17 22:41:13 +0800

    new update

--- app/src/main/java/com/charoenpokhandfoodph/Login.java
+++ app/src/main/java/com/charoenpokhandfoodph/Login.java
@@ -3,12 +3,16 @@ package com.charoenpokhandfoodph;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_ACTION_BAR_OVERLAY);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
     }
 }
--- app/src/main/java/com/charoenpokhandfoodph/MainActivity.java
+++ app/src/main/java/com/charoenpokhandfoodph/MainActivity.java
@@ -1,10 +1,27 @@
 package com.charoenpokhandfoodph;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityOptionsCompat;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Toast;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.DexterError;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.PermissionRequestErrorListener;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,5 +32,87 @@ public class MainActivity extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
+        Dexter.withActivity(this)
+                .withPermissions(
+                        Manifest.permission.INTERNET,
+                        Manifest.permission.ACCESS_WIFI_STATE,
+                        Manifest.permission.ACCESS_NETWORK_STATE
+//                        Manifest.permission.READ_SMS,
+//                        Manifest.permission.RECEIVE_SMS,
+//                        Manifest.permission.CAMERA,
+//                        Manifest.permission.CALL_PHONE
+                )
+                .withListener(new MultiplePermissionsListener() {
+                    @Override
+                    public void onPermissionsChecked(MultiplePermissionsReport report) {
+                        if (report.areAllPermissionsGranted()) {
+                            new Handler().postDelayed(() -> {
+                                Intent i = new Intent(getApplicationContext(), Login.class);
+                                ActivityOptionsCompat option = ActivityOptionsCompat.makeSceneTransitionAnimation(MainActivity.this,(findViewById(R.id.logo)),"logo");
+                                startActivity(i,option.toBundle());
+
+//                                    if(isLogin.equals("true")){
+////                                        function.intent(getApplicationContext(),Home.class);
+////                                        CustomIntent.customType(getApplicationContext(),config.ANIM);
+//                                        Intent i = new Intent(getApplicationContext(), newHome.class);
+//                                        ActivityOptionsCompat option = ActivityOptionsCompat.makeSceneTransitionAnimation(MainActivity.this,(findViewById(R.id.logo)),"logo");
+//                                        startActivity(i,option.toBundle());
+//                                    }
+//                                    else{
+//                                        Intent i = new Intent(getApplicationContext(), Login.class);
+//                                        ActivityOptionsCompat option = ActivityOptionsCompat.makeSceneTransitionAnimation(MainActivity.this,(findViewById(R.id.logo)),"logo");
+//                                        startActivity(i,option.toBundle());
+//                                    }
+
+                            },3000); // 3 second splash Screen
+
+                        }
+
+                        // check for permanent denial of any permission
+                        else if (report.isAnyPermissionPermanentlyDenied()) {
+                            // permission is denied permenantly, navigate user to app settings
+                            showSettingsDialog();
+                        }
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {
+                        token.continuePermissionRequest();
+                    }
+                })
+                .withErrorListener(new PermissionRequestErrorListener() {
+                    @Override
+                    public void onError(DexterError error) {
+                        Toast.makeText(getApplicationContext(), "Error occurred! ", Toast.LENGTH_SHORT).show();
+                    }
+                })
+
+                .onSameThread()
+                .check();
+
+
+
+
     }
+
+    private void showSettingsDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("Need Permissions");
+        builder.setMessage("This app needs permission to use this feature. You can grant them in app settings.");
+        builder.setPositiveButton("GOTO SETTINGS", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        builder.show();
+
+    }
+
 }
