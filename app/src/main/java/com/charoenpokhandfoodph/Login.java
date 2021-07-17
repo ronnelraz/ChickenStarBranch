81961034c88423fb6d72a7208ce5f4b8fa79ff06
ChickenstarBranch <japjaprazo@gmail.com>
2021-07-17 22:41:58 +0800

    new update

--- app/src/main/java/com/charoenpokhandfoodph/Login.java
+++ app/src/main/java/com/charoenpokhandfoodph/Login.java
@@ -11,8 +11,6 @@ public class Login extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_ACTION_BAR_OVERLAY);
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
     }
 }
