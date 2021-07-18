7ec9c7af60d5b8045a2234070f39c5316a321a6e
ChickenstarBranch <japjaprazo@gmail.com>
2021-07-18 09:54:22 +0800

    new update

--- app/src/main/java/com/charoenpokhandfoodph/Home.java
+++ app/src/main/java/com/charoenpokhandfoodph/Home.java
@@ -6,13 +6,16 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 import android.widget.Toolbar;
 
+import com.github.ivbaranov.mli.MaterialLetterIcon;
 import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.navigation.NavigationView;
 
@@ -23,22 +26,49 @@ public class Home extends AppCompatActivity {
     TextView textCartItemCount;
     int mCartItemCount = 10;
 
-    private DrawerLayout mDrawer;
+    private DrawerLayout drawerLayout;
     private MaterialToolbar toolbar;
-    private NavigationView nvDrawer;
-    private ActionBarDrawerToggle drawerToggle;
+    private NavigationView navigationView;
+    public static FragmentManager fragmentManager;
+    public static FrameLayout container;
+
+    TextView fullname,address;
+    MaterialLetterIcon iconx;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        MaterialToolbar toolbar = findViewById(R.id.toolbar);
+        toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nvView);
+        container = findViewById(R.id.flContent);
+
+        drawableMenu();
 
 
     }
 
+
+    protected  void drawableMenu(){
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.drawer_open,R.string.drawer_close);
+        toggle.getDrawerArrowDrawable().setColor(Color.parseColor("#ffffff"));
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        //fragment
+//        fragmentManager = getSupportFragmentManager();
+//        setFrag(new Tab1());
+
+
+        View headerView = navigationView.getHeaderView(0);
+        fullname = headerView.findViewById(R.id.fullname);
+        address = headerView.findViewById(R.id.address);
+        iconx = headerView.findViewById(R.id.iconx);
+    }
 
 
 
